c82abec81375dc5c9bcaa0b33301d510fe84c651
SiamAlSobari <siamqueen52@gmail.com>
2025-09-07 21:41:46 +0700

    feat: add auto-save and git status

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,6 +1,8 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    enable_git_status = true, -- ✅ ini biar ada M, U, dsb.
+    enable_diagnostics = true, -- kalau mau ada tanda warning/error di file
     filesystem = {
       filtered_items = {
         hide_dotfiles = false, -- biar file . (dotfiles) keliatan
