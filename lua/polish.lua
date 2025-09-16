4117b9d3a2559ff01d7f031bb0f05a003aa1dcbc
YammD123 <siamqueen52@gmail.com>
2025-09-16 20:13:16 +0700

    feat: menambahkan keymaps pindah baris tapi di mode insert

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,3 +1,6 @@
+-- Insert mode: move line up/down pakai Alt + panah (tanpa keluar dari insert mode)
+vim.keymap.set("i", "<A-Up>", "<C-o>:m .-2<CR><C-o>==", { desc = "Move line up (Insert mode)" })
+vim.keymap.set("i", "<A-Down>", "<C-o>:m .+1<CR><C-o>==", { desc = "Move line down (Insert mode)" })
 -- Normal mode: move line up/down pakai Alt + panah
 vim.keymap.set("n", "<A-Up>", ":m .-2<CR>==", { desc = "Move line up" })
 vim.keymap.set("n", "<A-Down>", ":m .+1<CR>==", { desc = "Move line down" })
